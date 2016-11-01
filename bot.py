e60e5ad149f3cc13157dee0d2e77a8629840cd5d
Adrian-Hall <muzioclavecin@gmail.com>
2016-11-01 10:08:36 -0400

    d

--- bot.py
+++ bot.py
@@ -32,7 +32,6 @@ def contains(origin, text):
                     vf = True
                 if a >= (len(text)-1):
                     loop2 = False
-                    vf = True
         if vf == False:
             soc += 1
             print ('Found occurance')
