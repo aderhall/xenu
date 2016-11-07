d66c6edc29810995c07190c6789035573a4a515c
Adrian-Hall <muzioclavecin@gmail.com>
2016-11-07 10:34:20 -0500

    sp

--- bot.py
+++ bot.py
@@ -292,7 +292,7 @@ def on_message(message):
             yield from client.send_message(message.channel, 'An invite code to add VictiBot to a server you manage has been sent to you via DM')
             yield from client.send_message(message.author, 'Add ViciBot to a server using this link: ')
             yield from client.send_message(message.author, 'https://discordapp.com/oauth2/authorize?client_id=231595610682294272&scope=bot&permissions=0')
-            yield from client.send_message(message.author, 'Note: You can only add VictiBot to a server if you have the \'Manage server\' privilage on it.')
+            yield from client.send_message(message.author, 'Note: You can only add VictiBot to a server if you have the \'Manage server\' privilege on it.')
         # List all servers that the bot can see
         elif msg.startswith(PREFIX + 'servers'):
             # Accumulation text variable
