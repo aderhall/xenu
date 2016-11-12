3880803e46c770e5bf2f331ef6676f95c99c0957
Adrian-Hall <muzioclavecin@gmail.com>
2016-11-12 09:54:20 -0500

    d

--- bot.py
+++ bot.py
@@ -381,7 +381,8 @@ def on_message(message):
         elif msg.startswith(PREFIX + 'define'):
             r = requests.get('https://github.com/adambom/dictionary/blob/master/dictionary.json')
             #try:
-            returnMsg = r.json[msg[8:]]
+            returnMsg = r.json[msg[8:].upper()]
+            print ('Definition: ' + returnMsg)
             #except:
             #returnMsg = 'Could not retrieve definition'
         # Respond to messages from dictionaries (to make code more efficient)
